f8563991c76d10f40ecf1a6f1ac38207b8626f23
jnielsen85 <thisisjn@gmail.com>
2020-08-07 10:47:40 +1000

    added page tab icon

--- gatsby-JNS Surfboards/src/html.js
+++ gatsby-JNS Surfboards/src/html.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import favicon from './assets/img/favicon.ico'
+import favicon from './assets/img/surfboard.ico'
 
 export default class HTML extends Component {
   render () {
